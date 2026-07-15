719183a6141f65dc1c0a993b93cf1a2bb8086c81
emiya-77 <golam.kibria1205@gmail.com>
2026-07-15 06:35:58 +0600

    Setup redux toolkit
    Signed-off-by: emiya-77 <golam.kibria1205@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import "./index.css"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { RouterProvider } from "react-router"
 import router from "./routes/index.tsx"
+import { Provider } from "react-redux"
+import { store } from "./redux/store.ts"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </ThemeProvider>
   </StrictMode>
 )
